33ba632e455116b53f5636006d2488824bcfe89c
RishikeshDeshpande <rishi.s.deshpande@gmail.com>
2023-10-25 17:58:56 +1100

    Using sets, vectors, and arrays to dynamically store floods and fires based on severity status. Playing around with adding complexities and challenges to the overall design of the solution :)

--- include/Rossenheimer/Navigator.h
+++ include/Rossenheimer/Navigator.h
@@ -13,7 +13,9 @@
 #include "Goal/GoalFire.h"
 #include "Goal/GoalFlood.h"
 #include <vector>
+#include <array>
 #include <iostream>
+#include <cmath>
 
 class Navigator {
     public:
@@ -21,8 +23,16 @@ class Navigator {
         ~Navigator();
         void MoveToGoal(int GoalNum);
         void SetGoal(double x, double y, double pose, int goalType);  
+        void SetGoal(int april_id, double x, double y);
+        void SortGoals();
     private:
+        static const int numPriorities = 3;
+
         std::vector<std::pair< Goal *, int>> Goals;
+
+        std::set<std::pair< int, std::pair<double, double>>> _ids_pos;
+        std::array< std::set <Goal *>, numPriorities> _priorityBook;
+
         int GoalNum;
         GoalBased* BaseGoal;
         GoalFire* FireGoal;
