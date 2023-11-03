6fccad7a1aa11ee2443944614176168270e541f4
acoo8177 <144605028+acoo8177@users.noreply.github.com>
2023-11-03 18:44:09 +1100

    Update Goal.h
    Commenting completed for Goal.h

--- include/Rossenheimer/Goal/Goal.h
+++ include/Rossenheimer/Goal/Goal.h
@@ -1,8 +1,12 @@
+// Header Guard
+
 #ifndef GOAL_H_
 #define GOAL_H_
 
+// Include header files
 #include "../Sensors/Sensor.h" 
 
+// Includes
 #include <ros/ros.h>
 #include <geometry_msgs/Point.h>
 #include <actionlib/client/simple_action_client.h>
@@ -10,7 +14,7 @@
 #include <move_base_msgs/MoveBaseGoal.h>
 #include <actionlib_msgs/GoalStatus.h>
 
-
+// Indexes for goal types
 enum
 {
     TYPE_BASE = 0,
@@ -18,18 +22,33 @@ enum
     TYPE_FLOOD
 };
 
+//---Goal Implementation---------------------------------------------------------
+// The Goal class is parent to the child classes of: GoalFire, GoalFlood and
+// GoalBased. The defined functions and member variables are used by the child
+// classes in order to execute the desired task for the desired class type
+
 class Goal
 {
     public:
+        //--Constructors and Destructors--
         Goal();
         Goal(double px, double py, double pz, double ox, double oy, double oz, double ow, int type, int id);
         virtual void actionTask() = 0; //
         ~Goal();
+
+        //--Functionality--
+        // Functions for obtaining goal type and position based on Camera and
+        // Odometer readings
         int GetType();
         double GetPosition(int req);
     protected:
+        // Member variables that define the goal
+
+        // Goal status (active/inactive)
         bool status;
-        double Target_x;
+
+        // Position variables
+        double Target_x;    
         double Target_y;
         double Target_z;
         double Pose_x;
@@ -37,6 +56,7 @@ class Goal
         double Pose_z;
         double Pose_w;
 
+        // Variables used to identify goal and type
         int Target_type;
         int april_id;
 
