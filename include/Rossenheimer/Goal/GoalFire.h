ae103fd25c47dd1b0b4a39c29bce97b706a43e4a
acoo8177 <144605028+acoo8177@users.noreply.github.com>
2023-11-03 18:44:57 +1100

    Update GoalFire.h
    Commenting completed for GoalFire.h

--- include/Rossenheimer/Goal/GoalFire.h
+++ include/Rossenheimer/Goal/GoalFire.h
@@ -1,15 +1,24 @@
+// Header guards
 #ifndef GOALFIRE_H_
 #define GOALFIRE_H_
 
+// Includes
 #include <ros/ros.h>
 #include "Goal.h"
 
+//---GoalFire Implementation---------------------------------------------------------
+// The GoalFire class is a child class to Goal, completing a separate actionTask
+// function to other goal types
 class GoalFire : public Goal
 {
     public:
+        // Constructors and Destructors
         GoalFire(double px, double py, double pz, double ox, double oy, double oz, double ow, int type, int id);
-        void actionTask();
         ~GoalFire();
+
+        // Redefined virtual function from parent class
+        void actionTask();
+        
     private:
 };
 
