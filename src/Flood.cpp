430674cccd01f6db4d224c497e3333f03e457542
acoo8177 <144605028+acoo8177@users.noreply.github.com>
2023-11-03 18:49:03 +1100

    Commenting completed for Flood.cpp

--- src/Flood.cpp
+++ src/Flood.cpp
@@ -1,7 +1,10 @@
+// Includes
 #include <ros/ros.h>
 
+// Include header files
 #include "../include/Rossenheimer/Goal/GoalFlood.h"
 
+//--Flood Object Constructor--
 GoalFlood::GoalFlood(double px, double py, double pz, double ox, double oy, double oz, double ow, int type, int id)
 {
     ROS_INFO("[CTor]: Flood created.");
@@ -16,17 +19,16 @@ GoalFlood::GoalFlood(double px, double py, double pz, double ox, double oy, doub
     
     Target_type = type;
     april_id = id;
-
 }
 
+//--Flood Object Destructor
 GoalFlood::~GoalFlood()
 {
     ROS_INFO("[DTor]: Flood blocked");
 } 
 
-
+//actionTask used to block floods when the goal is reached
 void GoalFlood::actionTask()
 {
     ROS_INFO("blocking Flood");
-    status = false;
 }
