b7a4c2a1c311f8ec0f531e9ee07d1ce653c8ed90
acoo8177 <144605028+acoo8177@users.noreply.github.com>
2023-11-03 18:44:34 +1100

    Update GoalBased.h
    Commenting completed for GoalBased.h

--- include/Rossenheimer/Goal/GoalBased.h
+++ include/Rossenheimer/Goal/GoalBased.h
@@ -1,18 +1,26 @@
+// Header guards
 #ifndef BASED_H_
 #define BASED_H_
 
+// Includes
 #include <ros/ros.h>
 #include "Goal.h"
 
+//---GoalBased Implementation---------------------------------------------------------
+// The GoalBased class is a child class to Goal, completing a separate actionTask
+// function to other goal types. There should only be one GoalBased object for the
+// duration of the program
 class GoalBased : public Goal
 {
     public:
+        // Constructors and Destructors
         GoalBased();
         GoalBased(double px, double py, double pz, double ox, double oy, double oz, double ow, int type, int id);
-        void actionTask();
         ~GoalBased();
-    private:
-        
+
+        // Redefined virtual function from parent class
+        void actionTask();
+    private:      
 };
 
 #endif
