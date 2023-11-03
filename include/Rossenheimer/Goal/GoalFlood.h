214cd65349b498aadef6f8153e0c902501ead947
acoo8177 <144605028+acoo8177@users.noreply.github.com>
2023-11-03 18:45:25 +1100

    Update GoalFlood.h
    Commenting completed for GoalFlood.h

--- include/Rossenheimer/Goal/GoalFlood.h
+++ include/Rossenheimer/Goal/GoalFlood.h
@@ -1,15 +1,24 @@
+// Includes
 #ifndef GOALFLOOD_H_
 #define GOALFLOOD_H_
 
+// Include header files
 #include <ros/ros.h>
 #include "Goal.h"
 
+//---GoalFire Implementation---------------------------------------------------------
+// The GoalFlood class is a child class to Goal, completing a separate actionTask
+// function to other goal types
 class GoalFlood : public Goal
 {
     public:
+        // Constructors and Destructors
         GoalFlood(double px, double py, double pz, double ox, double oy, double oz, double ow, int type, int id);
-        void actionTask();
         ~GoalFlood();
+
+        // Redefined virtual function from parent class
+        void actionTask();
+        
     private:
 };
 
